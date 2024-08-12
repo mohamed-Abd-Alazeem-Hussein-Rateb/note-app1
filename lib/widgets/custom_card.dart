35434dc824316d3d43558378ac994f2a36ebd62a
mohamed-Abd-Alazeem-Hussein-Rateb <mohamedabdlazeem36@gmail.com>
2024-08-12 09:14:27 +0300

    creat edit note view and edit note body view

--- lib/widgets/custom_card.dart
+++ lib/widgets/custom_card.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:notes/widgets/editbodyview.dart';
 
 class CustomCard extends StatelessWidget {
   const CustomCard({super.key, required this.title, required this.description, required this.date, required this.color});
@@ -8,58 +9,65 @@ class CustomCard extends StatelessWidget {
   final Color color;
   @override
   Widget build(BuildContext context) {
-    return Card(
-            child: Container(
-              padding: const EdgeInsets.symmetric(vertical: 30, horizontal: 16),
-              decoration: BoxDecoration(
-                color: color,
-                borderRadius: BorderRadius.circular(16.0),
-              ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                   Row(
-                    children: [
-                      Text(
-                        title,
-                        style:const TextStyle(
-                          fontSize: 22,
-                          fontWeight: FontWeight.bold,
+    return InkWell(
+      onTap: (){
+        Navigator.push(context, MaterialPageRoute(builder: (context){
+          return EditNoteBodyView();
+        } ));
+      },
+      child: Card(
+              child: Container(
+                padding: const EdgeInsets.symmetric(vertical: 30, horizontal: 16),
+                decoration: BoxDecoration(
+                  color: color,
+                  borderRadius: BorderRadius.circular(16.0),
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                     Row(
+                      children: [
+                        Text(
+                          title,
+                          style:const TextStyle(
+                            fontSize: 22,
+                            fontWeight: FontWeight.bold,
+                          ),
                         ),
+                       const Spacer(),
+                      InkWell(
+                        onTap: () {},
+                        child: const  Icon(
+                            Icons.delete,
+                            size: 30,
+                            color: Colors.black,
+                          ),
                       ),
-                     const Spacer(),
-                    InkWell(
-                      onTap: () {},
-                      child: const  Icon(
-                          Icons.delete,
-                          size: 30,
-                          color: Colors.black,
-                        ),
+                      ],
                     ),
-                    ],
-                  ),
-                 const SizedBox(height: 8),
-                  Text(
-                    description,
-                    style: TextStyle(
-                      fontSize: 16,
-                      color: Colors.black.withOpacity(0.6),
-                    ),
-                  ),
-                
-                  Align(
-                    alignment: Alignment.bottomRight,
-                    child: Text(
-                      date,
+                   const SizedBox(height: 8),
+                    Text(
+                      description,
                       style: TextStyle(
-                        fontSize: 14,
+                        fontSize: 16,
                         color: Colors.black.withOpacity(0.6),
                       ),
                     ),
-                  ),
-                ],
+                  
+                    Align(
+                      alignment: Alignment.bottomRight,
+                      child: Text(
+                        date,
+                        style: TextStyle(
+                          fontSize: 14,
+                          color: Colors.black.withOpacity(0.6),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
-          );
+    );
   }
 }
---
+++ lib/widgets/editbodyview.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class EditNoteBodyView extends StatelessWidget {
+  const EditNoteBodyView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column();
+  }
+}
