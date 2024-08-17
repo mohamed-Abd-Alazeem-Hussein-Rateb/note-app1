dbb03a29084aa6226568cbf99cca78c77162501b
mohamed-Abd-Alazeem-Hussein-Rateb <mohamedabdlazeem36@gmail.com>
2024-08-17 10:04:58 +0300

    colors list view part 2

--- lib/widgets/color_list_item.dart
+++ lib/widgets/color_list_item.dart
@@ -1,32 +1,65 @@
 import 'package:flutter/material.dart';
 
 class ColorItem extends StatelessWidget {
-  const ColorItem({super.key});
-
+  const ColorItem({super.key, required this.selected, required this.color, });
+  final bool selected;
+  final Color color;
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: const CircleAvatar(
-        radius: 36,
-        backgroundColor: Colors.blue,
-      ),
-    );
+    return selected
+        ? CircleAvatar(
+            radius: 40,
+            backgroundColor: Colors.white,
+            child: CircleAvatar(
+              radius: 33,
+              backgroundColor: color,
+            ),
+          )
+        : CircleAvatar(
+            radius: 36,
+            backgroundColor:color ,
+          );
   }
 }
-class ColorListView extends StatelessWidget {
+
+class ColorListView extends StatefulWidget {
   const ColorListView({super.key});
 
   @override
+  State<ColorListView> createState() => _ColorListViewState();
+}
+
+class _ColorListViewState extends State<ColorListView> {
+  int currentIndex = 0;
+  List<Color> colors = const [
+    Color(0xff05299E),
+    Color(0xff5E4AE3),
+    Color(0xff947BD3),
+    Color(0xffF0A7A0),
+    Color(0xffF26CA7),
+  ];
+  @override
   Widget build(BuildContext context) {
     return SizedBox(
-      height: 36*2,
+      height: 36 * 2,
       child: ListView.builder(
-        itemCount: 10,
+        itemCount: colors.length,
         scrollDirection: Axis.horizontal,
         itemBuilder: (context, index) {
-          return const ColorItem();
-        },  
+          return  InkWell(
+            onTap: () {
+              currentIndex = index;
+              setState(() {});
+            },
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10, ),
+              child: ColorItem(
+                color: colors[index],
+                selected: currentIndex == index,  // يمكن تغير اللون هنا حسب المتطلبات
+              ),
+            ),
+          );
+        },
       ),
     );
   }
